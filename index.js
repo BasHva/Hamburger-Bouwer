ab006e3045186290217ee3e720670362f6df0a7b
BasHva <145450292+BasHva@users.noreply.github.com>
2024-06-29 11:56:00 +0200

    29/06

--- index.js
+++ index.js
@@ -2,77 +2,55 @@ console.log("hamburger");
 
 // Constanten voor het selecteren van HTML-elementen
 const kaasElement = document.querySelector(".kaas-btn");
-const tomaatElement = document.querySelector(".tomaat-btn");
-const slaElement = document.querySelector(".sla-btn");
-const augurkElement = document.querySelector(".augurk-btn");
-const broodjeElement = document.querySelector(".hamburger-image");
 const rundElement = document.querySelector(".rund-btn");
 const ketchupElement = document.querySelector(".ketchup-btn");
-const toppings = ["sla", "tomaat", "kaas", "augurk", "rund", "ketchup"];
+const broodjeElement = document.getElementById("broodje");
 
-// Variabele om de status van de burger bij te houden
-let burgerStatus = 0;
+// Variabele houd huidige toppings bij
+let toppings = {
+  kaas: false,
+  rund: false,
+  ketchup: false,
+};
 
-// Functies om afbeelding van burger te veranderen bij toevoeging van andere toppings
-function veranderAfbeeldingKaas() {
-  if (burgerStatus === 0) {
-    broodjeElement.src = "images/kaas.png";
-    burgerStatus = 1;
-  } else {
-    broodjeElement.src = "images/hamburgerleeg.png";
-    burgerStatus = 0;
+// Functie om afbeelding broodje aan te passen basis van huidige toppings
+function updateHamburgerImage() {
+  let imageName = "hamburgerleeg.png";
+
+  if (toppings.kaas && toppings.rund && toppings.ketchup) {
+    imageName = "alletoppings.png";
+  } else if (toppings.kaas && toppings.rund) {
+    imageName = "kaasrund.png";
+  } else if (toppings.kaas && toppings.ketchup) {
+    imageName = "kaasketchup.png";
+  } else if (toppings.rund && toppings.ketchup) {
+    imageName = "rundketchup.png";
+  } else if (toppings.kaas) {
+    imageName = "kaas.png";
+  } else if (toppings.rund) {
+    imageName = "rund.png";
+  } else if (toppings.ketchup) {
+    imageName = "ketchup.png";
   }
+
+  broodjeElement.src = "images/" + imageName;
 }
 
-function veranderAfbeeldingTomaat() {
-  if (burgerStatus === 1) {
-    broodjeElement.src = "images/tomaatkaas.png";
-    burgerStatus = 2;
-  } else {
-    broodjeElement.src = "images/kaas.png";
-    burgerStatus = 1;
-  }
-}
+// Event listener voor klikken op de buttons
+kaasElement.addEventListener("click", function () {
+  toppings.kaas = !toppings.kaas; // Toggle de kaas topping
+  updateHamburgerImage(); // Update afbeelding van broodje
+});
 
-function veranderAfbeeldingSla() {
-  if (burgerStatus === 2) {
-    broodjeElement.src = "images/kaastomaatsla.png";
-    burgerStatus = 3;
-  } else {
-    broodjeElement.src = "images/tomaatkaas.png";
-    burgerStatus = 2;
-  }
-}
+rundElement.addEventListener("click", function () {
+  toppings.rund = !toppings.rund;
+  updateHamburgerImage();
+});
 
-function veranderAfbeeldingAugurk() {
-  if (burgerStatus === 3) {
-    broodjeElement.src = "images/alletoppings.png";
-    burgerStatus = 4;
-  } else {
-    broodjeElement.src = "images/kaastomaatsla.png";
-    burgerStatus = 3;
-  }
-}
-
-function veranderAfbeeldingRund() {
-  if (burgerStatus === 4) {
-    broodjeElement.src = "images/alletoppings.png";
-    burgerStatus = 5;
-  } else {
-    broodjeElement.src = "images/alletoppingsenrund.png";
-    burgerStatus = 4;
-  }
-}
-
-function veranderAfbeeldingKetchup() {
-  if (burgerStatus === 5) {
-    broodjeElement.src = "images/alletoppingsenrund.png";
-    burgerStatus = 6;
-  } else {
-    broodjeElement.src = "images/metketchup.png";
-    burgerStatus = 5;
-  }
-}
+ketchupElement.addEventListener("click", function () {
+  toppings.ketchup = !toppings.ketchup;
+  updateHamburgerImage();
+});
 
 // Functie om audio af te spelen
 function playAudio() {
@@ -80,25 +58,6 @@ function playAudio() {
   audioElement.play();
 }
 
-// https://noaheakin.medium.com/adding-sound-to-your-js-web-app-f6a0ca728984#:~:text=The%20simplest%20way%20to%20add,starts%20playing%20the%20current%20audio.
-// https://developer.mozilla.org/en-US/docs/Web/API/HTMLAudioElement/Audio|
-// https://stackoverflow.com/questions/9419263/how-to-play-audio
-// https://pixabay.com/sound-effects/search/jazz/
-
-// Event Listeners voor het klikken op knoppen
-kaasElement.addEventListener("click", veranderAfbeeldingKaas);
-tomaatElement.addEventListener("click", veranderAfbeeldingTomaat);
-slaElement.addEventListener("click", veranderAfbeeldingSla);
-augurkElement.addEventListener("click", veranderAfbeeldingAugurk);
-rundElement.addEventListener("click", veranderAfbeeldingRund);
-ketchupElement.addEventListener("click", veranderAfbeeldingKetchup);
-
-// Loop door elk element in de toppings array en log het naar de console
-toppings.forEach(function (topping) {
-  console.log(topping);
-});
-
-// Event listener voor het klikken op de afspeelknop
 document.getElementById("playButton").addEventListener("click", playAudio);
 
 //BRONVERMELDING:
@@ -106,3 +65,4 @@ document.getElementById("playButton").addEventListener("click", playAudio);
 // https://www.figma.com/community/file/1190436932512042976
 // https://pixabay.com/sound-effects/search/jazz/
 // https://developer.mozilla.org/en-US/docs/Web/API/HTMLAudioElement/Audio
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/conditionals / https://www.javascripttutorial.net/javascript-if-else/
